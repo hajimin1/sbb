7b09a17a40d01657036abcffe3249219322ec257
Jimin Ha <smjmjh26@naver.com>
2023-07-31 11:55:58 +0900

    apply pagination

--- src/main/java/com/mygroup/sbb/question/QuestionController.java
+++ src/main/java/com/mygroup/sbb/question/QuestionController.java
@@ -2,20 +2,19 @@ package com.mygroup.sbb.question;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.data.domain.Page;
 
 import com.mygroup.sbb.answer.AnswerForm;
 
 import jakarta.validation.Valid;
+import org.springframework.validation.BindingResult;
 
-import java.util.List;
-
-import lombok.RequiredArgsConstructor;// 생성자를 자동으로 만들어주는 것
+import lombok.RequiredArgsConstructor;
 
 @RequestMapping("/question")
 @RequiredArgsConstructor
@@ -25,15 +24,14 @@ public class QuestionController {
     private final QuestionService questionService;
 
     @GetMapping("/list")
-    public String list(Model model) {
-        List<Question> questionList = this.questionService.getList();
-        model.addAttribute("questionList", questionList);
+    public String list(Model model, @RequestParam(value="page", defaultValue="0") int page) {
+        Page<Question> paging = this.questionService.getList(page);
+        model.addAttribute("paging", paging);
         return "question_list";
     }
 
     @GetMapping(value = "/detail/{id}")
-    public String detail(Model model, @PathVariable("id") Integer id, AnswerForm answerForm
-    ) { // 해당 컨트롤러 넣었을 때 찾아주는 것
+    public String detail(Model model, @PathVariable("id") Integer id, AnswerForm answerForm) {
         Question question = this.questionService.getQuestion(id);
         model.addAttribute("question", question);
         return "question_detail";
@@ -52,6 +50,4 @@ public class QuestionController {
         this.questionService.create(questionForm.getSubject(), questionForm.getContent());
         return "redirect:/question/list"; // 질문 저장후 질문목록으로 이동
     }
-
-
 }
